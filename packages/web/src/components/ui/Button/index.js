300420cf55a1e6c6232ec9d48c66775884d6738a
Tom von Geldern <tvongeldern@Toms-MacBook-Air.local>
2020-04-08 14:27:58 -0500

    Basic input styling

--- packages/web/src/components/ui/Button/index.js
+++ packages/web/src/components/ui/Button/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { arrayOf, node, oneOfType } from 'prop-types';
+import { arrayOf, bool, node, oneOfType } from 'prop-types';
+import cn from 'classnames';
 import styles from './styles.scss';
 
-export function Button({ children, ...rest }) {
-	return <button className={styles.button} {...rest}>{children}</button>;
+export function Button({
+	children,
+	className,
+	primary,
+	secondary,
+	tertiary,
+	quaternary,
+	...rest
+}) {
+	const buttonClass = cn(styles.button, className, {
+		[styles.primary]: primary,
+		[styles.secondary]: secondary,
+		[styles.tertiary]: tertiary,
+		[styles.quaternary]: quaternary,
+	});
+	return <button className={buttonClass} {...rest}>{children}</button>;
 }
 
 Button.propTypes = {
 	children: oneOfType([node, arrayOf(node)]),
+	primary: bool,
+	secondary: bool,
+	tertiary: bool,
+	quaternary: bool,
 };
 
 Button.defaultProps = {
 	children: null,
+	primary: false,
+	secondary: false,
+	tertiary: false,
+	quaternary: false,
 };
