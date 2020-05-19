405c5cc9b6fa448b7ba099f9034ac975b655e17a
Tom von Geldern <tvongeldern@Toms-MacBook-Air.local>
2020-05-19 16:34:54 -0500

    Extract iam roles tf file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,21 +1,3 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "IAMforTopstitchLambda"
-
-  assume_role_policy = jsonencode({
-		"Version": "2012-10-17",
-		"Statement": [
-			{
-				"Action": "sts:AssumeRole",
-				"Principal": {
-					"Service": "lambda.amazonaws.com"
-				},
-				"Effect": "Allow",
-				"Sid": ""
-			}
-		]
-	})
-}
-
 resource "aws_lambda_function" "cognito_pre_signup_trigger" {
   filename      = ".build/cognito_pre_signup.zip"
   function_name = "topstitch_${terraform.workspace}_lambda_cognito_pre_signup"
